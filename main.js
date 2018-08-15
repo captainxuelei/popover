1084893f0fdcf473d806a1b9e9be9f81d90056b5
captainxuelei <xw14510@163.com>
2018-08-15 13:14:49 +0800

    浮层第一个版本

---
+++ main.js
@@ -0,0 +1,16 @@
+$(clickMe).on('click', function(){
+  if(popover.style.display==='none'){
+    $(popover).show()
+    $(document).one('click', function(){
+      $(popover).hide()
+    })
+  }else{
+    $(popover).hide()
+  }
+  
+})
+$(wrapper).on('click', function(x){
+  x.stopPropagation()
+})
+
+
